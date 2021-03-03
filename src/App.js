1f366581f3ffd05217659f36857cc1ed39db38a8
Ajeet jha <jhaajeetcs@gmail.com>
2021-03-03 15:17:14 +0530

    nav bar component design and install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+import Navbar from './components/layout/Navbar';
 function App() {
   return (
-    <div className="App">
-     hi there
-    </div>
+    <Router>
+      <div className="App">
+      <Navbar />
+      </div>
+    </Router>
   );
 }
 
