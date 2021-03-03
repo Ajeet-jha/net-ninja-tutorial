bf271326f2e279f072dd559e09d026209417a9a8
Ajeet jha <jhaajeetcs@gmail.com>
2021-03-03 16:58:41 +0530

    created dummy products in product reducre and pass in through dashboad

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,10 @@
 const initialState  = {
-
+    projects: [
+        {id:'1', title: 'help me find peach', content: 'blah blah blah'},
+        {id:'2', title: 'Hi there', content: 'blah blah blah'},
+        {id:'3', title: 'come here', content: 'blah blah blah'},
+        {id:'4', title: 'go there', content: 'blah blah blah'},
+    ]
 }
 
 const projectReducer = (state=initialState, action) => {
